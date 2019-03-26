5eb51f6fd7c9d15aee37f180aba1a5c795c26bfe
Yechiel Tauber <yt4262839@gmail.com>
2019-03-26 15:59:01 -0400

    Changes

--- HelloGit/Controllers/HomeController.cs
+++ HelloGit/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace HelloGit.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello changes.";
 
             return View();
         }
